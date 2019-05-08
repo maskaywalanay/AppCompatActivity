914575ef63c1562a6e0d098debccd3d7c862be95
mwalanay@gmail.com <mwalanay@gmail.com>
2019-05-08 20:56:27 +0700

    AppCompatActivity

---
+++ AppCompatActivity/app/src/main/java/illegaller/aida/dev/art/appcompat/MainActivity.java
@@ -0,0 +1,14 @@
+package illegaller.aida.dev.art.appcompat;
+
+import android.os.*;
+import android.support.v7.app.*;
+
+public class MainActivity extends AppCompatActivity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
